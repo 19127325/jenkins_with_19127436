c0ade4edb224af324eed7fa1f318da359115513d
An <hoangan2626@gmail.com>
2022-01-15 16:58:26 +0700

    add new member

--- basic_java.java
+++ basic_java.java
@@ -3,6 +3,6 @@ public class basic_java {
         System.out.println("Assignment 3");
         System.out.println("19127325 - Nguyen Huu Hoang An");
         System.out.println("19127436 - Tang Tuong Khang");
-        System.out.println("New member");
+        System.out.println("New member: Ho Dang Khoa");
     }
 }
