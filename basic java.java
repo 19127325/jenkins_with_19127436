4887da54a1d594c63787166c465e0484e4d59f2c
An <hoangan2626@gmail.com>
2022-01-15 13:29:35 +0700

    Group file

---
+++ basic java.java
@@ -0,0 +1,7 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Assignment 3");
+        System.out.println("19127325 - Nguyen Huu Hoang An");
+        System.out.println("19127436 - Tang Tuong Khang");
+    }
+}
