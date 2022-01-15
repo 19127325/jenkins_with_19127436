a8366e09774179937d00215b3835c814862a3d04
An <hoangan2626@gmail.com>
2022-01-15 13:50:14 +0700

    update file java

--- basic_java.java
+++ basic_java.java
@@ -3,5 +3,6 @@ class HelloWorld {
         System.out.println("Assignment 3");
         System.out.println("19127325 - Nguyen Huu Hoang An");
         System.out.println("19127436 - Tang Tuong Khang");
+        System.out.println("New member");
     }
 }
